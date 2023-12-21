e07017ac55394f137f7e819b311af8cbbe93ad1f
umararana1122 <umara.rana@techbolic.com>
2023-12-21 11:18:53 +0500

    added password reset authentication

--- lib/Screens/verification_Code.dart
+++ lib/Screens/verification_Code.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:practiceapp/Screens/via_method.dart';
 import 'package:practiceapp/Widgets/genericTextWidget.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
